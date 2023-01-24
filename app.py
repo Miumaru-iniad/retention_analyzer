ffa427784797a68eb7e86a26463d7664a6f9b68a
Miumaru-iniad <s1f102200637@iniad.org>
2023-01-24 23:14:36 +0900

    make plot

--- app.py
+++ app.py
@@ -14,3 +14,18 @@ for f in files:
     fname = f.split()[2]
     fnames.append(fname)
     print(fname)
+
+x_datas = range(0,100)
+
+for a in files:
+    rows = []
+    print(a)
+    with open(a,encoding="utf-8") as f:   
+        reader = csv.reader(f)
+        rows = [row for row in reader]
+    header = rows.pop(0)
+    data = np.float_(np.array(rows).T)
+    plt.xlabel(header[0])
+    plt.ylabel(header[1])
+    plt.plot(data[0], data[1], linestyle='solid', marker='o')
+    plt.show()
