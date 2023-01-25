10d78277d7866b03e6b47d34647da74fac1e1be2
Miumaru-iniad <s1f102200637@iniad.org>
2023-01-25 23:40:47 +0900

    percentage to seconds

--- app.py
+++ app.py
@@ -31,10 +31,11 @@ for a in files:
     rivals = [x+y for x,y in zip(data[1],data[2])]
     data = list(data)
     data.append(rivals)
-    print(len(data[0]))
+    length =len(data[0])
+    print(length)
     #ポップアップで動画の秒数を取得
     layout =[[sg.Text(fname+"の動画時間を教えて下さい")],
-             [sg.InputText(default_text="00",key='m'),sg.Text("分"),sg.InputText(default_text="00",key='s'),sg.Text("秒")],
+             [sg.InputText(default_text="0",key='m',size=(10,10)),sg.Text("分"),sg.InputText(default_text="01",key='s',size=(10,10)),sg.Text("秒")],
              [sg.Button('決定')]]
     window = sg.Window('動画の秒数確認', layout)
     while True:
@@ -46,13 +47,21 @@ for a in files:
             break
     window.close()
     #分秒を秒に変換してからデータ数に合わせて分割し、dataにappendする
+    #喰った　 2:24
+    #旅じまい  2:51
+    #準備できた？　1:36
     print(second)
+    seconds =[(second*x)/length for x in range(length)]
+    print(seconds)
+    data.append(seconds)
+
+
     data_lst.append(data)
     plt.title(fnames[count],fontname="MS Gothic")
     plt.xlabel("time (second)")
     plt.ylabel("retation")
-    plt.plot(data[0], data[1], linestyle='solid',label=fnames[count])
-    plt.plot(data[0], data[3], linestyle='solid',label='rivals of ' + fnames[count])
+    plt.plot(data[4], data[1], linestyle='solid',label=fnames[count])
+    plt.plot(data[4], data[3], linestyle='solid',label='rivals of ' + fnames[count])
     plt.legend(loc = 'upper right',prop={"family":"MS Gothic"})
     plt.show()
     count+=1
@@ -61,7 +70,7 @@ plt.title("Songs Retation")
 plt.xlabel("time (second)")
 plt.ylabel("retation")
 for x in range(len(files)):
-    plt.plot(data_lst[x][0], data_lst[x][1], linestyle='solid',label=fnames[x])
+    plt.plot(data_lst[x][4], data_lst[x][1], linestyle='solid',label=fnames[x])
 plt.legend(loc = 'upper right',prop={"family":"MS Gothic"})
 plt.show()
 
